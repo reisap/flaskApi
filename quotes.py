91d15b415a8efdb1379deecec9c32aed42642bdc
Reisa Prasaptaraya <r.prasaptaraya@gmail.com>
2018-02-07 11:10:58 +0700

    update first commit

---
+++ quotes.py
@@ -0,0 +1,19 @@
+
+def funny_quotes():
+	return[{
+		"author":"Reisa prasaptaraya",
+		"quote":"Waktunya pemimpi jadi pemimpin"
+		},
+		{
+		"author":"Reisa prasaptaraya",
+		"quote":"Tidak ada yang terlambat"
+		},
+		{
+		"author":"Reisa prasaptaraya",
+		"quote":"Tahu bulat di goreng dadakan"
+		},
+		{
+		"author":"Reisa prasaptaraya",
+		"quote":"Kang Iweng Joget"
+		}
+	]
